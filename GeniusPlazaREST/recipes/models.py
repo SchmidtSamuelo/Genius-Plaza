039bc32587f1403cd796667feb2aada85c72a8a6
SchmidtSamuel <samfinnian@gmail.com>
2019-03-19 11:07:42 -0400

    Bugfix
    Fixed an error when attempting to import from a different model due to syntax.

--- GeniusPlazaREST/recipes/models.py
+++ GeniusPlazaREST/recipes/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Recipe(models.Model):
     dishName = models.CharField(max_length = 255)
-    creatorEmail = models.CharField(max_length = 255)
+    creator = models.ForeignKey(User, on_delete = CASCADE)
--- GeniusPlazaREST/steps/models.py
+++ GeniusPlazaREST/steps/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from recipes.models import Recipe
 
 # Create your models here.
 
