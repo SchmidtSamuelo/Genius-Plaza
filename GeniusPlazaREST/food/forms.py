acaf6f3418724aa8dbe0f20b3e3b0efe85a78e95
SchmidtSamuel <samfinnian@gmail.com>
2019-03-21 20:19:35 -0400

    Create Recipes update
    Create recipes takes a dropdown for the creator, this allows people to submit recipes for other users as long as they are in the Django default Users model (because Gordon Ramsay probably won't be using this site).

--- GeniusPlazaREST/food/forms.py
+++ GeniusPlazaREST/food/forms.py
@@ -2,14 +2,10 @@ from django import forms
 from food.models import Recipe
 
 class recipeCreationForm(forms.ModelForm):
-    #recipe_name = forms.CharField()
-    #recipe_creator_username = forms.CharField()
+
     class Meta:
         model = Recipe
         fields = '__all__'
-        # uncomment recipe_name and recipe_creator_username and
-        # use fields = ('recipe_name', 'recipe_creator_username')
-        # if '__all__' doesn't work
 
 #class recipeDeleteForm(forms.ModelForm):
     #recipe_ID = forms.CharField()
